7cfad99378c71a6e112e5a025b876e2b35b49e73
Josef-TL <j.tonnesenlucas@gmail.com>
2024-11-26 17:07:43 +0100

    db setup

--- db_service.py
+++ db_service.py
@@ -7,15 +7,15 @@ load_dotenv()
 db_path = os.getenv('DB_PATH')
 
 def init():
-  ##
-  with sqlite3.connect(db_path) as con:
-    cur = con.cursor()
-    cur.execute('''
-    CREATE TABLE IF NOT EXISTS kunder (
-      cpr integer [primary key]  
-      navn string 
-      tlf string 
-      email string
-      adresse string 
-    ''')
-  con.commit()
+    with sqlite3.connect(db_path) as conn:
+      c = conn.cursor()
+      c.execute('''CREATE TABLE IF NOT EXISTS users (
+                cpr INTEGER PRIMARY KEY, 
+                navn TEXT, 
+                tlf TEXT,
+                email TEXT,
+                adresse TEXT)
+            ''')
+      
+    conn.commit()
+    
